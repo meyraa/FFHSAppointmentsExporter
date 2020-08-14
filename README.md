09e39b8970f2753fc76fa65db349db3263d90d93
meyraa <huemeyra.yilmaz@outlook,.com>
2020-08-14 14:20:49 +0200

    add AppointmentsExporterGoogle.js

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # FFHSAppointmentsExporter
-For exporting appointments in FFHS-Moodle
+To export the calendar, go to [Veranstaltungs- & Prüfungstermine](https://students.ffhs.ch/campus/#!app/smartdesign.campus.appointment) and click on `Meine Termine`.
+Now you can open up the developer tools and paste the script in `AppointmensExporterGoogle.js` into the console. It should download a CSV that you can import into Google Calendar.
---
+++ src/AppointmentsExporterGoogle.js
@@ -0,0 +1,30 @@
+const header = encodeURIComponent("Subject, Start Date, Start Time, End Date, End Time, Location\n");
+
+const appointmentsCollection = document.getElementsByClassName("v-table-table")[0].getElementsByTagName("tr");
+const appointments = Array.prototype.slice.call(appointmentsCollection);
+
+const csvContent = appointments.map((appointment) => {
+	const columnsCollection = appointment.getElementsByTagName("td");
+	const columns = Array.prototype.slice.call(columnsCollection);
+	columns.pop();
+	
+	const content = [];
+	columns.forEach(({ innerText }, index) => {
+		if (index === 0 || index === 4) {
+			content.push(innerText.split(",").join(" -"));
+		}
+		
+		if (index === 1 || index === 2) {
+			content.push(...innerText.split(", "));
+		}
+	});
+	
+	return content.join(",");
+}).join("\n");
+
+const link = document.createElement("a");
+link.setAttribute("href", "data:attachment/csv," + header + encodeURIComponent(csvContent));
+link.setAttribute("download", "MeineTermine.csv");
+document.body.appendChild(link); // Required for FF
+
+link.click();
